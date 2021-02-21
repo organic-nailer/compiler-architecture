609e0a1782d91458dfd838a4067ddf5eba3ed787
速川湧気 <hykwyuk0125@gmail.com>
2021-02-21 22:00:48 +0900

    SLR1 parser

---
+++ src/main/kotlin/parser/SLR1Parser.kt
@@ -0,0 +1,66 @@
+package parser
+
+class SLR1Parser(
+    private val transitionMap: Map<Pair<String, String>, SLR1ParserGenerator.TransitionData>
+) {
+    fun parse(input: List<String>): Node? {
+        println("input: ${input.joinToString("")}")
+        val stack = ArrayDeque<Pair<String,String>>()// state,token
+        val nodeStack = ArrayDeque<Node>()
+        var parseIndex = 0
+        var accepted = false
+        stack.addFirst("I0" to "")
+        while(parseIndex < input.size || stack.isNotEmpty()) {
+            val transition = transitionMap[stack.first().first to input[parseIndex]]
+            when(transition?.kind) {
+                SLR1ParserGenerator.TransitionKind.SHIFT -> {
+                    stack.addFirst(transition.value!! to input[parseIndex])
+                    parseIndex++
+                    nodeStack.addFirst(Node(stack.first().second, stack.first().second, mutableListOf()))
+                }
+                SLR1ParserGenerator.TransitionKind.REDUCE -> {
+                    val rule = transition.rule!!
+                    val newNode = Node(rule.left, null, mutableListOf())
+                    for(t in rule.right.reversed()) {
+                        if(stack.first().second == t) {
+                            stack.removeFirst()
+                            newNode.children.add(nodeStack.removeFirst())
+                        }
+                        else {
+                            throw Exception("還元時エラー $rule, $stack, $t")
+                        }
+                    }
+                    transitionMap[stack.first().first to rule.left]?.value?.let {
+                        stack.addFirst(it to rule.left)
+                    } ?: kotlin.run {
+                        throw Exception("還元shiftエラー $rule, $stack")
+                    }
+                    nodeStack.addFirst(newNode)
+                }
+                SLR1ParserGenerator.TransitionKind.ACCEPT -> {
+                    accepted = true
+                    break
+                }
+                else -> {
+                    throw Exception("パースエラー: $stack, $parseIndex")
+                }
+            }
+        }
+        if(!accepted) {
+            println("受理されませんでした ${nodeStack.first()}")
+            return null
+        }
+        return nodeStack.first()
+    }
+
+    data class Node(
+        val kind: String,
+        val value: String?,
+        val children: MutableList<Node>
+    ) {
+        fun print(indent: String) {
+            println(indent + this.kind)
+            this.children.forEach { it.print("$indent  ") }
+        }
+    }
+}
--- src/test/kotlin/SLR1ParserGeneratorTest.kt
+++ src/test/kotlin/SLR1ParserGeneratorTest.kt
@@ -1,5 +1,6 @@
 import org.junit.Test
 import parser.LR0ParserGenerator
+import parser.SLR1Parser
 import parser.SLR1ParserGenerator
 
 class SLR1ParserGeneratorTest {
@@ -20,6 +21,26 @@ class SLR1ParserGeneratorTest {
         generator.printClosureMap()
         generator.printGotoMap()
         generator.printTransitionMap()
+        assert(true)
+    }
+
+    @Test
+    fun parseTest() {
+        val generator = SLR1ParserGenerator(listOf(
+            SLR1ParserGenerator.ProductionRuleData("E", "E + T".tokenize()),
+            SLR1ParserGenerator.ProductionRuleData("E", "T".tokenize()),
+            SLR1ParserGenerator.ProductionRuleData("T", "T * F".tokenize()),
+            SLR1ParserGenerator.ProductionRuleData("T", "F".tokenize()),
+            SLR1ParserGenerator.ProductionRuleData("F", "( E )".tokenize()),
+            SLR1ParserGenerator.ProductionRuleData("F", "i".tokenize()),
+        ), "E")
+        generator.printClosureMap()
+        generator.printGotoMap()
+        generator.printTransitionMap()
+        val parser = SLR1Parser(generator.transitionMap)
+        val node = parser.parse("i * i + i $".tokenize())
+        assert(node != null)
+        node?.print("")
         assert(true)
     }
 }
